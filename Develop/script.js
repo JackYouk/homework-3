23b6b2bedc94367b475c28f1ad6f3b2e29625b5b
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-05-10 21:38:52 -0700

    added prompts for user input and if statements to check validity of user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,43 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+
+//generates random password
+function generatePassword() {
+  // user input for password length
+  const passwordLength = prompt("Please enter a password length of at least 8 characters and no more than 128 characters.");
+  // checks if password meets length criteria
+  if (passwordLength < 8 || passwordLength > 128){
+    alert("Please enter a valid password length. Valid password lengths are greater than 8 characters and less than 128 characters.");
+    generatePassword();
+  }
+
+  // user input for character types
+  const isCharactersLowercase = confirm("Include lowercase characters?");
+  const isCharactersUppercase = confirm("Include uppercase characters?");
+  const isCharactersNumeric = confirm("Include numeric characters?");
+  const isCharactersSpecial = confirm("Include special characters?");
+  // checks if at least one character type is selected
+  if (!isCharactersLowercase && !isCharactersUppercase && !isCharactersNumeric && !isCharactersSpecial){
+    alert("Please select at least one character type.");
+    generatePassword();
+  }
+
+  // password generator using if and for loops here
+  const password = "";
+  
+
+  // returns generated password
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
