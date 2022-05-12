08f7a0489667fade14d3eda806be61edd546c0d9
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-05-11 17:02:34 -0700

    added functions for generating random characters, finished genNum. Also added arrays for letters and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,41 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-//generates random password
+//array of letters
+const letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// generates random Uppercase character
+function genUp(){
+
+}
+
+// generates random Lowercase character
+function genLow(){
+
+}
+
+// generates random Numeric character
+function genNum(){
+  let num = Math.random()*10;
+  num = Math.round(num);
+
+  return num;
+}
+
+// array of special characters
+const specialArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+// generates random special character
+function genSpec(){
+  let spec = specialArray[0];
+
+  return spec;
+}
+
+// generates random password
 function generatePassword() {
   // user input for password length
-  const passwordLength = prompt("Please enter a password length of at least 8 characters and no more than 128 characters.");
+  let passwordLength = prompt("Please enter a password length of at least 8 characters and no more than 128 characters.");
   // checks if password meets length criteria
   if (passwordLength < 8 || passwordLength > 128){
     alert("Please enter a valid password length. Valid password lengths are greater than 8 characters and less than 128 characters.");
@@ -12,10 +43,10 @@ function generatePassword() {
   }
 
   // user input for character types
-  const isCharactersLowercase = confirm("Include lowercase characters?");
-  const isCharactersUppercase = confirm("Include uppercase characters?");
-  const isCharactersNumeric = confirm("Include numeric characters?");
-  const isCharactersSpecial = confirm("Include special characters?");
+  let isCharactersLowercase = confirm("Include lowercase characters?");
+  let isCharactersUppercase = confirm("Include uppercase characters?");
+  let isCharactersNumeric = confirm("Include numeric characters?");
+  let isCharactersSpecial = confirm("Include special characters? (!, @, #, $, %, ^, &, *)");
   // checks if at least one character type is selected
   if (!isCharactersLowercase && !isCharactersUppercase && !isCharactersNumeric && !isCharactersSpecial){
     alert("Please select at least one character type.");
@@ -23,7 +54,7 @@ function generatePassword() {
   }
 
   // password generator using if and for loops here
-  const password = "";
+  let password = "";
   
 
   // returns generated password
